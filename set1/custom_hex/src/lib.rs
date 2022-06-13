10c4001adc085d838f408046d1eb48728420bca2
Roman <ackhtariev@gmail.com>
2022-06-13 17:30:33 -0400

    implement base16 encode

--- set1/custom_hex/src/lib.rs
+++ set1/custom_hex/src/lib.rs
@@ -30,6 +30,27 @@ pub fn decode(value: &[u8]) -> Result<Vec<u8>, String> {
     Ok(result)
 }
 
+fn encode_byte(b: u8) -> Result<u8, String> {
+    if b <= 9 {
+        return Ok(b + 48);
+    } else if b >= 10 && b <= 15  {
+        return Ok(97 + b - 10);
+    }
+    return Err(format!("{} does not translate to hex", b));
+}
+
+// encode encodes raw bytes to base16 according to RFC-4648.
+pub fn encode(value: &[u8]) -> Result<Vec<u8>, String> {
+    let mut result: Vec<u8> = Vec::with_capacity(value.len() * 2);
+
+    for b in value {
+        result.push(encode_byte(b >> 4 & 0b1111)?);
+        result.push(encode_byte(b & 0b1111)?);
+    }
+
+    Ok(result)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -79,6 +100,95 @@ mod tests {
 
         let actual_output = decode(input).unwrap();
         assert_eq!(&expected_output as &[u8], &actual_output as &[u8]);
+    }
+
+    #[test]
+    fn encode_byte_zero() {
+        const EXPECTED_OUTPUT: u8 = b'0';
+        let input: u8 = 0b0;
+
+        let actual_output = encode_byte(input).unwrap();
+        assert_eq!(EXPECTED_OUTPUT, actual_output);
+    }
+
+    #[test]
+    fn encode_byte_eleven() {
+        const EXPECTED_OUTPUT: u8 = b'a';
+        let input: u8 = 0xa;
+
+        let actual_output = encode_byte(input).unwrap();
+        assert_eq!(EXPECTED_OUTPUT, actual_output);
+    }
+
+    #[test]
+    fn encode_byte_sixteen_invalid() {
+        let input: u8 = 0b10000;
+
+        let err = encode_byte(input).unwrap_err();
+        assert_eq!(err, format!("{} does not translate to hex", input))
+    }
+
+    #[test]
+    fn encode_empty() {
+        const EXPECTED_OUTPUT: &[u8] = b"";
+        let input: &[u8] = b"";
+
+        let actual_output = encode(input).unwrap();
+        assert_eq!(EXPECTED_OUTPUT, &actual_output);
+    }
+
+    #[test]
+    fn encode_f() {
+        const EXPECTED_OUTPUT: &[u8] = b"66";
+        let input: &[u8] = b"f";
+
+        let actual_output = encode(input).unwrap();
+        assert_eq!(EXPECTED_OUTPUT, &actual_output);
+    }
+
+    #[test]
+    fn encode_fo() {
+        const EXPECTED_OUTPUT: &[u8] = b"666f";
+        let input: &[u8] = b"fo";
+
+        let actual_output = encode(input).unwrap();
+        assert_eq!(EXPECTED_OUTPUT, &actual_output);
+    }
+
+    #[test]
+    fn encode_foo() {
+        const EXPECTED_OUTPUT: &[u8] = b"666f6f";
+        let input: &[u8] = b"foo";
+
+        let actual_output = encode(input).unwrap();
+        assert_eq!(EXPECTED_OUTPUT, &actual_output);
+    }
+
+    #[test]
+    fn encode_foob() {
+        const EXPECTED_OUTPUT: &[u8] = b"666f6f62";
+        let input: &[u8] = b"foob";
+
+        let actual_output = encode(input).unwrap();
+        assert_eq!(EXPECTED_OUTPUT, &actual_output);
+    }
+
+    #[test]
+    fn encode_fooba() {
+        const EXPECTED_OUTPUT: &[u8] = b"666f6f6261";
+        let input: &[u8] = b"fooba";
+
+        let actual_output = encode(input).unwrap();
+        assert_eq!(EXPECTED_OUTPUT, &actual_output);
+    }
+
+    #[test]
+    fn encode_foobar() {
+        const EXPECTED_OUTPUT: &[u8] = b"666f6f626172";
+        let input: &[u8] = b"foobar";
+
+        let actual_output = encode(input).unwrap();
+        assert_eq!(EXPECTED_OUTPUT, &actual_output);
     }
 }
 
